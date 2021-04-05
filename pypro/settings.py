5e9346e3a50b0181646d00d1a68f890f4e72b06f
thiagobaia <thiagobaia@outlook.com>
2021-04-05 17:51:58 -0300

    collect static remove duplicate

--- pypro/settings.py
+++ pypro/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'collectfast',
     'django.contrib.staticfiles',
+    'collectfast',
     'pypro.base',
 ]
 
